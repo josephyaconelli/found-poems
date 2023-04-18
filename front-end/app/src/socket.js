cbc79f579d5a250cdfab9b1d29f15114997b8016
Joseph Yaconelli <josephyaconelli@gmail.com>
2023-04-17 18:45:45 -0700

    update w/ propper envvars

--- front-end/app/src/socket.js
+++ front-end/app/src/socket.js
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client'
 
-const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://10.0.0.43:4000';
+const URL = process.env.NODE_ENV === 'production' ? process.env.API_ADDRESS : 'http://10.0.0.43:4000';
 
 export const socket = io(URL)
--- server/api/app.js
+++ server/api/app.js
@@ -86,8 +86,8 @@ io.on('connection', (socket) => {
   });
 });
 
-server.listen(3000, () => {
-  console.log('listening on *:3000');
+server.listen(80, () => {
+  console.log('listening on *:80');
 });
 
-io.listen(4000);
+io.listen(3000);
